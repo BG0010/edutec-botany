3eeca27dbae7acbb987f226928ffed53664a5fb3
Vinícius <borgesvini11@gmail.com>
2023-10-07 22:44:00 -0300

    Mudança do quiz mais interativo

---
+++ quiz/js/questions.js
@@ -0,0 +1,120 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Qual é o processo pelo qual as plantas convertem a luz solar em energia?",
+    answer: "Fotossíntese",
+    options: [
+      "Fotossíntese",
+      "Respiração",
+      "Transpiração",
+      "Germinação"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual é a parte da planta responsável pela absorção de água e nutrientes do solo?",
+    answer: "Raiz",
+    options: [
+      "Flor",
+      "Raiz",
+      "Caule",
+      "Folhas"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Como as plantas carnívoras obtêm nutrientes essenciais que geralmente não estão disponíveis no solo?",
+    answer: "Comendo insetos",
+    options: [
+      "Absorvendo minerais diretamente da água",
+      "Através da fotossíntese",
+      "Através da polinização",
+      "Comendo insetos"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qual é a principal função dos estômatos nas folhas das plantas?",
+    answer: "Transpiração",
+    options: [
+      "Proteção contra predadores",
+      "Armazenamento de nutrientes",
+      "Transpiração",
+      "Fotossíntese"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual é o nome da estrutura reprodutiva das gimnospermas que se assemelha a uma pequena pinha?",
+    answer: "Cone",
+    options: [
+      "Cone",
+      "Flor",
+      "Fruto",
+      "Semente"
+    ]
+  },
+    {
+        numb: 6,
+        question: "Qual é o processo pelo qual as plantas florescidas produzem sementes sem a necessidade de fertilização?",
+        answer: "Apomixia",
+        options: [
+            "Polinização",
+            "Disseminação",
+            "Apomixia",
+            "Germinação"
+
+        ]
+    },
+    {
+        numb: 7,
+        question: "Qual é o nome da substância verde nas plantas que desempenha um papel crucial na fotossíntese?",
+        answer: "Clorofila",
+        options: [
+            "Floema",
+            "Xilema",
+            "Celulose",
+            "Clorofila"
+            
+        ]
+    },
+    {
+        numb: 8,
+        question: "Qual é o termo usado para se referir à transferência de pólen de uma flor para outra da mesma espécie?",
+        answer: "Polinização",
+        options: [
+            "Germinação",
+            "Fecundação",
+            "Polinização",
+            "Fotossíntese"
+            
+        ]
+    },
+    {
+        numb: 9,
+        question: "Qual é a parte da planta que geralmente armazena energia na forma de amido?",
+        answer: "Raiz",
+        options: [
+            "Folha",
+            "Flor",
+            "Caule",
+            "Raiz"
+            
+        ]
+    },
+    {
+        numb: 10,
+        question: "Qual é o termo para a perda de água na forma de vapor pelas folhas das plantas?",
+        answer: "Transpiração",
+        options: [
+            "Transpiração",
+            "Evaporação",
+            "Absorção",
+            "Respiração"
+            
+        ]
+    }
+
+
+];
